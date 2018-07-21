0b6b246cda99553ecd5ba0dfcf96174ef7b166fe
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-07-21 12:46:23 +0430

    Add CryptoAnalysis class

---
+++ Code/CryptoAnalysis.py
@@ -0,0 +1,15 @@
+class CryptoAnalysis(object):
+    def __init__(self, data):
+        self.data = data
+        pass
+
+    def SMA(self, period=3):
+        pass
+
+    def EMA(self):
+        pass
+
+    def BBands(self):
+        pass
+
+
