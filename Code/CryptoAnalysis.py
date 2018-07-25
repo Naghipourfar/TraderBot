ef7e9a5e63cde6338fb347c6b545a0017823887f
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-07-25 23:00:15 +0430

    Start CryptoAnalysis Package

--- Code/CryptoAnalysis.py
+++ Code/CryptoAnalysis.py
@@ -1,9 +1,19 @@
 from pandas import ewma
+import pandas as pd
+import numpy as np
+
 
 class CryptoAnalysis(object):
     def __init__(self, data):
         self.data = data
-        pass
+        if isinstance(self.data, pd.DataFrame):
+            print("Data is pandas Dataframe.")
+        elif isinstance(self.data, np.ndarray):
+            print("Data is numpy array.")
+        elif isinstance(self.data, pd.Series):
+            print("Data is pandas Series.")
+        else:
+            raise ValueError("The input data must be numpy array, pandas DataFrame or Series.")
 
     def SMA(self, period=3):
         pass
@@ -15,3 +25,5 @@ class CryptoAnalysis(object):
         pass
 
 
+if __name__ == '__main__':
+    c = CryptoAnalysis([1, 2, 3])
