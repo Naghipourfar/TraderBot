731ded10e750d00c382c226ebefe4c317ef00f06
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-07-25 23:46:43 +0430

    Add some good functions

--- Code/model.py
+++ Code/model.py
@@ -1,50 +1,27 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-import requests
 import json
-import time
+import os
 
 import keras
-import keras.backend as K
-from keras.layers import Dropout, Dense, LSTM, Input
-from keras.models import Model
-
-from sklearn.model_selection import train_test_split, TimeSeriesSplit
+import numpy as np
+import pandas as pd
+import requests
+from keras.layers import Dropout, Dense, LSTM
+from keras.models import Sequential
 from sklearn.preprocessing import normalize
-
-from datetime import datetime
-from datetime import timedelta
-
-
-def line_plot(line1, line2, label1=None, label2=None, title=''):
-    fig, ax = plt.subplots(1, figsize=(25, 15))
-    ax.plot(line1, label=label1, linewidth=2)
-    ax.plot(line2, label=label2, linewidth=2)
-
-    for i, j in zip(line1, line2):
-        ax.plot([], [], )
-
-    ax.set_ylabel('price [USD]', fontsize=14)
-    ax.set_title(title, fontsize=18)
-    ax.legend(loc='best', fontsize=18)
-    plt.show()
 
 
 def normalize_data(data):
-    return normalize(data, norm='l2', axis=1, copy=True)
+    return normalize(data, norm='max', axis=1, copy=True)
 
 
-def create_model(input_features, output_neurons=1):  # TODO: Has to be checked!
-    input_layer = Input(shape=(input_features,))
-    lstm_layer = LSTM(20, input_shape=(input_features,))(input_layer)
-    lstm_layer = Dropout(0.5)(lstm_layer)
-    dense_layer = Dense(output_neurons, activation=keras.activations.linear)
-
-    model = Model(input_layer, dense_layer)
-    model.compile(loss=keras.losses.mape, optimizer=keras.optimizers.nadam)
-
+def create_model(x_data, layers):
+    model = Sequential()
+    model.add(LSTM(layers[0], input_shape=(x_data.shape[1], x_data.shape[2]), return_sequences=True))
+    model.add(Dropout(0.5))
+    model.add(LSTM(layers[1], return_sequences=False))
+    model.add(Dropout(0.5))
+    model.add(Dense(layers[-1], activation='linear'))
+    model.compile(loss=keras.losses.mse, optimizer="rmsprop")
     return model
 
 
@@ -54,26 +31,64 @@ def download_data(coin):
     hist = pd.DataFrame(json.loads(res.content)['Data'])
     hist = hist.set_index('time')
     hist.index = pd.to_datetime(hist.index, unit='s')
-    hist.head()
     return hist
 
 
+def load_data(filename, sequence_len=100, output_cols=None, test_split=0.2):
+    raw_data = pd.read_csv(filename)
+
+    if raw_data.columns.__contains__('Date'):
+        raw_data.drop(['date'], axis=1)
+    else:
+        deleted_cols = []
+        # for col in raw_data.columns:
+        #     if isinstance(raw_data.iloc[0, col], str):  # is either symbol or date
+        #         deleted_cols.append(col)
+        raw_data.drop(deleted_cols, axis=1)
+
+    raw_data_values = raw_data.values
+
+    samples = []
+    for index in range(0, raw_data.shape[0] - sequence_len):
+        samples.append(raw_data_values[index:index + sequence_len])
+
+    samples = np.array(samples)
+    print("data has shape\t:\t", samples.shape)
+
+    if output_cols:
+        test_size = int(test_split * samples.shape[0])
+        x_data = np.array(samples[:, :, :-1])
+        y_data = np.array(samples[:, :, -1])
+
+        x_train = x_data[:-test_size]
+        y_train = y_data[:-test_size]
+
+        x_test = x_data[test_size:]
+        y_test = y_data[test_size:]
+
+        return x_train, y_train, x_test, y_test
+    else:
+        return None, None, None, None
+
+
 def main():
-    coin = 'BTC'
-    data = download_data(coin)
-    y_data = data['close']
-    x_data = data.drop(['close'], axis=1)
+    filename = "./data.csv"
+    if not os.path.exists(filename):
+        coin = 'BTC'
+        data = download_data(coin)
+        data.to_csv(filename)
+        x_train, y_train, x_test, y_test = load_data(filename, sequence_len=50, output_cols=[5])
+    else:
+        x_train, y_train, x_test, y_test = load_data(filename, sequence_len=50, output_cols=[5])
 
-    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.1, shuffle=False)
-
-    model = create_model(data.shape[1], output_neurons=1)
-
+    model = create_model(x_train, layers=[50, 50, 1])
     model.fit(x=x_train,
               y=y_train,
-              epochs=50,
-              batch_size=128,
-              validation_data=(x_test, y_test),
-              verbose=2)
+              batch_size=1,
+              epochs=5,
+              verbose=2,
+              validation_split=0.1,
+              shuffle=False)
 
 
 if __name__ == '__main__':
